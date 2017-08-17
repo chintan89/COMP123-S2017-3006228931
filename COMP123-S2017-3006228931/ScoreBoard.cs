2fb67f67714fef137b1c0154139e13e4dd58a79c
chintan <chintan@MYPERSONAL>
2017-08-17 13:57:56 -0400

    ScoreBoardClass Created

---
+++ COMP123-S2017-3006228931/ScoreBoard.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Name - Chintan Patel
+ * Student # 300622893
+ * Date - Aug 17, 2017
+ * Description - Pick Higest Card
+ * Version: 0.2 ScoreBoardClass Created
+ */
+namespace COMP123_S2017_3006228931
+{
+    class ScoreBoard
+    {
+        private TextBox _finalScoreTextBox;
+        private int _score;
+        private TextBox _scoreTextBox;
+        private int _time;
+        private TextBox _timeTextBox;
+    }
+    public TextBox FinalScoreTextBox
+    {
+        get
+        {
+            return this._finalscoretextbox;
+        }
+        set
+        {
+            this._finalscoretextbox = value;
+            this.score = Covert.ToInt32(this.FinalScoreTextBox.text)
+        }
+    }
+
+}
--- COMP123-S2017-3006228931/SplashForm.Designer.cs
+++ COMP123-S2017-3006228931/SplashForm.Designer.cs
@@ -38,6 +38,9 @@ namespace COMP123_S2017_3006228931
         {
             this.components = new System.ComponentModel.Container();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblwelcome = new System.Windows.Forms.Label();
+            this.lblwelcome2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // timer1
@@ -46,6 +49,34 @@ namespace COMP123_S2017_3006228931
             this.timer1.Interval = 3000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(375, 173);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // lblwelcome
+            // 
+            this.lblwelcome.AutoSize = true;
+            this.lblwelcome.Location = new System.Drawing.Point(259, 160);
+            this.lblwelcome.Name = "lblwelcome";
+            this.lblwelcome.Size = new System.Drawing.Size(61, 13);
+            this.lblwelcome.TabIndex = 1;
+            this.lblwelcome.Text = "300622893";
+            // 
+            // lblwelcome2
+            // 
+            this.lblwelcome2.AutoSize = true;
+            this.lblwelcome2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblwelcome2.Location = new System.Drawing.Point(247, 160);
+            this.lblwelcome2.Name = "lblwelcome2";
+            this.lblwelcome2.Size = new System.Drawing.Size(49, 13);
+            this.lblwelcome2.TabIndex = 2;
+            this.lblwelcome2.Text = "welcome";
+            // 
             // SplashForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -53,6 +84,9 @@ namespace COMP123_S2017_3006228931
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(600, 500);
             this.ControlBox = false;
+            this.Controls.Add(this.lblwelcome2);
+            this.Controls.Add(this.lblwelcome);
+            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -60,11 +94,15 @@ namespace COMP123_S2017_3006228931
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SplashForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblwelcome;
+        private System.Windows.Forms.Label lblwelcome2;
     }
 }
