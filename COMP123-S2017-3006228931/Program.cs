d5463b9284ca835618f5d6764819248d52a6b702
chintan <chintan@MYPERSONAL>
2017-08-17 12:50:14 -0400

    Add project files.

---
+++ COMP123-S2017-3006228931/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * Name:
+ * Date:
+ * StudentID:
+ * Description:
+ * Version:
+ */
+
+namespace COMP123_S2017_3006228931
+{
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new PickHighestCardForm());
+        }
+    }
+}
